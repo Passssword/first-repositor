dcb22ad30ca8f5bd84699a2bf4abdee62e1f2a9c
Ilya Stepanenko <Stepanenko.my-mail@yandex.ru>
2020-11-23 17:01:41 +1100

    add button logic

--- components/anim_box_js/js/script.js
+++ components/anim_box_js/js/script.js
@@ -1,13 +1,19 @@
 /* JavaScript */
+
+let clickCounter = 0;
+
 // Получаем кнопку, блоки
 
-document.querySelector('.animboxbutton').onclick=animBox;
-// buttonMagic.onclick = animBox;
+let buttonMagic = document.querySelector('.anim_box_button');
+buttonMagic.onclick = animBox;
 let massiveBoxes = document.querySelectorAll ('.anim_box');   // Получаем массив блоков
+
 
 function animBox() {
 	massiveBoxes[0].classList.add('animBlock');
 	massiveBoxes[0].addEventListener('animationend', () => {
 		massiveBoxes[0].classList.remove('animBlock')
+		clickCounter++;
+		buttonMagic.innerHTML = "Active Box: " + clickCounter;
 	});
 }
