19ebfff59f0d250e44d56e0391d6c31d738d3dcd
Ilya Stepanenko <Stepanenko.my-mail@yandex.ru>
2020-11-23 16:00:53 +1100

    add animation blocks to dirr/components/anim_box_js/style.css

--- components/anim_box_js/js/script.js
+++ components/anim_box_js/js/script.js
@@ -1,1 +1,13 @@
 /* JavaScript */
+// Получаем кнопку, блоки
+
+document.querySelector('.animboxbutton').onclick=animBox;
+// buttonMagic.onclick = animBox;
+let massiveBoxes = document.querySelectorAll ('.anim_box');   // Получаем массив блоков
+
+function animBox() {
+	massiveBoxes[0].classList.add('animBlock');
+	massiveBoxes[0].addEventListener('animationend', () => {
+		massiveBoxes[0].classList.remove('animBlock')
+	});
+}
