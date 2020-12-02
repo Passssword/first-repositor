7ad5d98d3aa7db9ae7f5fcd239c221622930c6f6
Ilya Stepanenko <Stepanenko.my-mail@yandex.ru>
2020-12-02 13:09:54 +1100

    mirror changes

---
+++ components/anim_box_js/js/mutation.js
@@ -0,0 +1,5 @@
+let mutationObj = document.querySelector('.mutation_object');
+let mutationButton = document.querySelector('.butt_mutation');
+mutationButton.onclick = function function_name(argument) {
+	// body...
+}
