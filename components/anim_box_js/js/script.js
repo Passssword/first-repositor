64701a35f185c3f774435dce654c48d44180ed6a
Ilya <Stepanenko.my-mail@yandex.ru>
2020-12-09 23:15:37 +1100

    add worked animation to components/anim_box_js

--- components/anim_box_js/js/script.js
+++ components/anim_box_js/js/script.js
@@ -5,13 +5,15 @@ let clickCounter = 0;
 // Получаем кнопку, блоки
 
 let buttonMagic = document.querySelector('.anim_box_button');
-buttonMagic.onclick = animBox;
 let massiveBoxes = document.querySelectorAll ('.anim_box');   // Получаем массив блоков
 
+// buttonMagic.onclick = animBox;
 
-function animBox() {
+
+buttonMagic.onclick = function animBox() {
 	if (clickCounter < massiveBoxes.length-1) {
-		function (moveBox, plusCounter);
+		moveBox(addEventAnimationend);
+	}
 	else {
 		clickCounter = 0;
 	}
@@ -23,16 +25,22 @@ function animBox() {
 // 	}
 // }
 
-function plusCounter() {
-	clickCounter++;
+function removeAnimation () {
+	massiveBoxes[clickCounter].classList.remove("animBlock");
+	console.log(massiveBoxes[clickCounter].classList);
+	buttonMagic.innerHTML = "Active Box: " + clickCounter;
+	removeEventAnimationend();
 }
-function moveBox() {
-	massiveBoxes[clickCounter].classList.add('animBlock');
-	massiveBoxes[clickCounter].addEventListener('animationend', () => {
-		massiveBoxes[clickCounter].classList.remove("animBlock");
-		console.log(massiveBoxes[clickCounter].classList);
-		buttonMagic.innerHTML = "Active Box: " + clickCounter;
-	});
-	// clickCounter++;
+function addEventAnimationend (param_removeAnimation) {
+	massiveBoxes[clickCounter].addEventListener('animationend', removeAnimation);
+}
+function removeEventAnimationend() {
+	massiveBoxes[clickCounter].removeEventListener('animationend', removeAnimation)
+	clickCounter++;
 	console.log(clickCounter);
+}
+
+function moveBox(param_addEventAnimationend) {
+	massiveBoxes[clickCounter].classList.add('animBlock');
+	param_addEventAnimationend(removeAnimation);
 }
