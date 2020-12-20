241b5131e1b012f62485c65b5842080846c3f5f8
Ilya <Stepanenko.my-mail@yandex.ru>
2020-12-21 09:35:53 +1100

    working slider

--- components/draft/mando_slider/js/script.js
+++ components/draft/mando_slider/js/script.js
@@ -1,12 +1,41 @@
 
-let clickcounter = 0;
+let clickCounter = 0;
 let buttonNext = document.querySelector('.butt_next');
 let massiveCardPerson = document.querySelectorAll('.card_penson');
 
-buttonNext.onclick = function nextSlide() {
-	massiveCardPerson[clickcounter].classList.add('animTextIn');
-	massiveCardPerson[clickcounter].addEventListener('animationend', stopAnimation);
+// Задаем начальное расположение первого слайда
+massiveCardPerson[0].style.transform = 'translate(10px, 20px)';
+
+buttonNext.onclick = function () {
+	if (clickCounter < massiveCardPerson.length){
+		// showNextSlide();
+		removeCurrentSlide();
+	}
+	else {
+		clickCounter = 0;
+	}
+}
+function showNextSlide() {
+	buttonNext.disabled = true;
+	massiveCardPerson[clickCounter+1].classList.add('animImgIn');
+	massiveCardPerson[clickCounter+1].addEventListener('animationend', stopAnimation);
+}
+function removeCurrentSlide() {
+	buttonNext.disabled = true;
+	massiveCardPerson[clickCounter].classList.add('animImgOut');
+	massiveCardPerson[clickCounter].addEventListener('animationend', setLocationImgOut);
+	setTimeout(showNextSlide, 700);
+}
+
+function setLocationImgOut() {
+	massiveCardPerson[clickCounter].style.transform = 'translate(600px, 20px)';
+	massiveCardPerson[clickCounter].classList.remove('animImgOut');
+	massiveCardPerson[clickCounter].removeEventListener('animationend', setLocationImgOut);
 }
 function stopAnimation() {
-	massiveCardPerson[clickcounter].style.transform = 'translate(10px, 20px)';
+	massiveCardPerson[clickCounter+1].style.transform = 'translate(10px, 20px)';
+	massiveCardPerson[clickCounter+1].classList.remove('animImgIn');
+	massiveCardPerson[clickCounter+1].removeEventListener('animationend', stopAnimation);
+	clickCounter++;
+	buttonNext.disabled = false;
 }
